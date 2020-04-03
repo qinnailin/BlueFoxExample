f465b77038dcb456b613d73346a311e71b770b5e
qinnailin <qinnailin@126.com>
2020-04-03 09:56:26 +0800

    创建

---
+++ Bluefox.Base.Mvc.Demo/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿/************************************************************************
+* Copyright (c) 2020 All Rights Reserved.
+*命名空间：Bluefox.Base.Mvc.Demo.Controllers
+*文件名： ValuesController
+*创建人： 覃乃林
+*创建时间：2020/4/3 9:50:04
+*描述
+************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bluefox.Base.Mvc.Demo.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+
+    }
+}
